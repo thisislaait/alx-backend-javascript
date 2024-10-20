34f2cb66ec1e2f96b9c13be035328d8b314ab4ef
thisislaait <ogadi.ugo@gmail.com>
2024-10-20 13:50:45 -0700

    fix lint issues

--- 0x00-ES6_basic/eslint.config.js
+++ 0x00-ES6_basic/eslint.config.js
@@ -1,19 +1,25 @@
 export default [
   {
-      ignores: ['node_modules/**'],
+    ignores: ['node_modules/**'],
   },
   {
-      files: ['**/*.js'],
-      languageOptions: {
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-      },
-      rules: {
-          'no-var': 'error',
-          'prefer-const': 'error',
-          'semi': ['error', 'always'],
-          'quotes': ['error', 'single'],
-          'no-unused-vars': 'warn',
-      },
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-unused-vars': 'warn',
+      'indent': ['error', 2, { 'SwitchCase': 1 }],
+      'no-multiple-empty-lines': ['error', { 'max': 1 }],
+      'eol-last': ['error', 'always'],
+      'block-scoped-var': 'error',
+      'no-trailing-spaces': 'error', // Complete the rule here
+    },
   },
 ];
+
