637b999b71eddc1458abd61a452b8df21409e8f2
thisislaait <ogadi.ugo@gmail.com>
2024-10-20 12:56:45 -0700

    update constant.js file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,3 @@
-// 0-constants.js
-
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
@@ -15,4 +13,3 @@ export function taskFirst() {
   
     return combination;
   }
-  
